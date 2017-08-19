906789195b2bba342861f3320c18b5328f4a05fa
ZerepW <A01273687@itesm.mx>
2017-08-19 17:53:45 -0500

    Creando Index

--- servidor.js
+++ servidor.js
@@ -1,7 +1,15 @@
-var http = require('http');
+var http = require('http'); //MODULO HTTP
+var fs = require('fs'); // MODULO FInLE SYSTEM
+
+fs.readFileSync("./index.html");
+
+// req = solicitud del usuario
+// res = respuesta del servidor
+// Se ejecuta la funcion cada vez que el navegador hace la peticion
+// Se pueden crear N servidores en distintos puertos
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hello World!'); //Cierra la conexion de la peticion
     console.log("Funciona el servidor");
 }).listen(7777);
