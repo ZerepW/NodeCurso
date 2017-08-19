f39637e293360bb76d18570f7d50592dcd8585fb
ZerepW <A01273687@itesm.mx>
2017-08-19 18:07:23 -0500

    Programación Asíncrona

--- servidor.js
+++ servidor.js
@@ -1,7 +1,8 @@
 var http = require('http'); //MODULO HTTP
 var fs = require('fs'); // MODULO FInLE SYSTEM
 
-fs.readFileSync("./index.html");
+
+
 
 // req = solicitud del usuario
 // res = respuesta del servidor
@@ -9,7 +10,12 @@ fs.readFileSync("./index.html");
 // Se pueden crear N servidores en distintos puertos
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!'); //Cierra la conexion de la peticion
+    // Leer archivo de manera Asincrona, 
+    //recibe como parametros el Path y un callback que tiene de parametros
+    //error y datos o archivo
+    fs.readFile("./index.html",function(err, data){       
+        res.write(data)
+        res.end("Hola"); //Cierra la conexion de la peticion
+    }); 
     console.log("Funciona el servidor");
 }).listen(7777);
